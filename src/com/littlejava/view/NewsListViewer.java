e1ad3893b8a4d96e19d01b3461a09d4816821b63
YangYang <yangzi531703512@gmail.com>
2018-04-25 09:19:27 +0800

    遍历 map

--- src/com/littlejava/view/NewsListViewer.java
+++ src/com/littlejava/view/NewsListViewer.java
@@ -3,6 +3,7 @@ package com.littlejava.view;
 import com.littlejava.model.News;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class NewsListViewer {
     private ArrayList<News> newsList;
@@ -13,11 +14,12 @@ public class NewsListViewer {
 
     public void display() {
         for (News news : newsList) {
-            System.out.println("-----------------------------------------------");
+            System.out.println("----------------------------------------------------------------------------------------------");
             System.out.println("|Title| " + news.getTitle());
             System.out.println("|Content| " + news.getContent());
-            System.out.println("|related| " + news.getRelated());
-
+            for(Map.Entry<String, String> entry : news.getRelated().entrySet()) {
+                System.out.println("|related| " + entry.getKey() + "   " + entry.getValue());
+            }
             //填写你的代码
         }
     }
